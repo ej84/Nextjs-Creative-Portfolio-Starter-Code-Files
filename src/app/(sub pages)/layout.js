0131dd3f4315d5638d426828a1b46d8552fcd816
RjCoder <ej84@njit.edu>
2024-10-16 16:00:02 -0400

    Updating contact page.

--- src/app/(sub pages)/layout.js
+++ src/app/(sub pages)/layout.js
@@ -2,7 +2,7 @@ import HomeBtn from "../components/HomeBtn";
 
 export default function SubPagesLayout({ children }) {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center px-16 py-20">
+    <main className="flex min-h-screen flex-col items-center justify-center px-16 py-32">
       <HomeBtn />
       {children}
     </main>
--- src/app/components/about/index.jsx
+++ src/app/components/about/index.jsx
@@ -1,9 +1,77 @@
-import React from 'react'
+import React from "react";
+import clsx from "clsx";
+
+const ItemLayout = ({ children, className }) => {
+  return (
+    <div
+      className={clsx(
+        "custom-bg p-8 rounded-xl flex items-center justify-center space-y-8",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
 
 const AboutDetails = () => {
-    return (
-        <div>index</div>
-    )
-}
+  return (
+    <section className="py-20 w-full ">
+      <div className="grid grid-cols-12 gap-8 w-full">
+        <ItemLayout className={"col-span-8 row-span-2 flex-col items-start"}>
+          <h2 className="text-2xl text-left w-full capitalize">
+            Architect of Enchantment
+          </h2>
+          <p className="font-light">
+            My journey in web development is powered by an array of mystical
+            tools and languages, with JavaScript casting the core of my
+            enchantments. I wield frameworks like React.js and Next.js with
+            precision, crafting seamless portals (websites) that connect realms
+            (users) across the digital universe. The ancient arts of the
+            Jamstack empower me to create fast, secure, and dynamic experiences,
+            while my design skills ensure every creation is not only functional
+            but visually captivating. Join me as I continue to explore new
+            spells and technologies to shape the future of the web.
+          </p>
+        </ItemLayout>
+        <ItemLayout className={"col-span-4 text-accent"}>
+          <p className="font-semibold w-full text-left text-5xl">
+            25+ <sub className="font-semibold text-base">clients</sub>
+          </p>
+        </ItemLayout>
+        <ItemLayout className={"col-span-4 text-accent"}>
+          <p className="font-semibold w-full text-left text-5xl">
+            4+{" "}
+            <sub className="font-semibold text-base">years of experience</sub>
+          </p>
+        </ItemLayout>
+        <ItemLayout className={"col-span-4 !p-0"}>
+          <img
+            src="https://github-readme-stats.vercel.app/api/top-langs?username=codebucks27&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false"
+            className="w-full h-auto"
+            alt="CodeBucks"
+            loading="lazy"
+          />
+        </ItemLayout>
+        <ItemLayout className={"col-span-8 !p-0"}>
+          <img
+            src="https://github-readme-stats.vercel.app/api?username=codebucks27&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false"
+            className="w-full h-auto"
+            alt="CodeBucks"
+            loading="lazy"
+          />
+        </ItemLayout>
+        <ItemLayout className={"col-span-full"}>
+          <img
+            src="https://skillicons.dev/icons?i=html,js,php,css,tailwind,jquery,bootstrap,nextjs,python,docker,git,github,vscode"
+            className="w-full h-auto"
+            alt="CodeBucks"
+            loading="lazy"
+          />
+        </ItemLayout>
+      </div>
+    </section>
+  );
+};
 
 export default AboutDetails;
