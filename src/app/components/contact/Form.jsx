9c5e41592c7d7d1923861c28f36510c10b1f0105
ej84 <ej84@njit.edu>
2024-10-16 21:19:33 -0400

    .

--- src/app/components/contact/Form.jsx
+++ src/app/components/contact/Form.jsx
@@ -20,17 +20,26 @@ export default function Form() {
         type="text"
         placeholder="Name"
         {...register("Name", { required: true, maxLength: 80 })}
+        className="custom-input custom-bg"
       />
       <input
         type="email"
         placeholder="email"
         {...register("email", { required: true, pattern: /^\S+@\S+$/i })}
+        className="custom-input custom-bg"
       />
       <textarea
+        placeholder="message"
         {...register("message", { required: true, max: 256, min: 50 })}
+        className="custom-input custom-bg"
       />
 
-      <input type="submit" />
+      <input value="Cast your message"
+        className="px-10 py-4 rounded-md shadow-lg bg-background border border-accent/30 border-solid
+        hover:shadow-glass-sm backdrop-blur-sm text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50
+        cursor-pointer capitalize"
+        type="submit"
+      />
     </form>
   );
 }
